8f505fa42b4571c37fd60dbf07e654db7a402ad9
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2025-07-09 20:21:25 +0530

    routes

--- medium/backend/src/index.ts
+++ medium/backend/src/index.ts
@@ -1,49 +1,79 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
-import { PrismaClient } from './generated/prisma'
+import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { jwt } from 'hono/jwt'
+import { sign, verify } from 'hono/jwt'
 
-
-
-
-const app = new Hono()
-app.use(logger())
 
 const app = new Hono<{
   Bindings: {
-    DATABASE_URL: string,
+    DATABASE_URL: string
     JWT_SECRET: string,
   }
-}>();
+}>()
+app.use(logger())
 
-app.post('/api/v1/signuo', async (c) => {
+app.post('/api/v1/signup', async (c) => {
   const prisma = new PrismaClient({
     datasourceUrl: c.env?.DATABASE_URL,
   }).$extends(withAccelerate())
-  const body = c.req.json()
+
+  const body = await c.req.json()
+
   try {
     const user = await prisma.user.create({
       data: {
         email: body.email,
-        password: body.password
+        password: body.password,
+        post: ""
       }
     });
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET)
+    return c.json({ jwt })
 
-    return c.text('jwt here')
   } catch (e) {
     return c.status(403);
   }
 })
 
-app.post('/api/v1/signin', (c) => {
+app.post('/api/v1/signin', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate())
+  
+  const id = c.req.param('id')
+
+  const body = await c.req.json()
+  const user = prisma.user.findMany({
+    where: {
+      email: body.email
+    }
+  })
+  if (!user) {
+    c.status(403)
+    return c.json({ error: "user not found" })
+  }
+  const jwt = await sign({ id: id }, c.env.JWT_SECRET)
   return c.text("SignIn route")
 })
 
-app.get("/api/v1/blog/:id", (c) => {
+app.get("/api/v1/blog/:id", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate())
   const id = c.req.param('id')
-  // return c.text(id);
-  return c.text('get blog route')
+
+  const post = await prisma.post.findMany({
+    where: {
+      authorId: id
+    }
+  })
+
+  if (!post) {
+    c.status(404)
+    return c.json({ error: "blog not found" })
+  }
+  return c.json(post)
 })
 
 app.post('/api/v1/blog', (c) => {
