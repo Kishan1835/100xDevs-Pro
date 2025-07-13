c3aff513751bdc0e3caa8d48569862fb3cbef84c
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2025-07-14 01:59:27 +0530

    added:sigin/signup

--- medium/frontend/src/pages/SignUp.tsx
+++ medium/frontend/src/pages/SignUp.tsx
@@ -1,12 +1,17 @@
-import { Quotes } from "../components/Quotes"
+import { Quotes } from "../components/Quotes";
+import { Auth } from "../components/Auth";
 
 export const SignUp = () => {
-    return <div>
-        <div className=" grid grid-cols-2">
-            <div>
-                
-           </div>
-             <Quotes/>
+  return (
+    <div>
+      <div className=" grid grid-cols-2">
+        <div>
+          <Auth />
         </div>
+        <div className="invisible md:visible">
+          <Quotes />
+        </div>
+      </div>
     </div>
-}
+  );
+};
