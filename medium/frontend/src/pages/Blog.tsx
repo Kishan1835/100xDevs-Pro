afca050ad14f465eae3824130b8ab8d102ccf383
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2025-07-14 12:35:33 +0530

    added:blog page

--- medium/frontend/src/pages/Blog.tsx
+++ medium/frontend/src/pages/Blog.tsx
@@ -1,7 +1,20 @@
 export const Blog = () => {
-    return <div>
-        <div>
-            Blog
-        </div>
+  return (
+    <div className=" grid grid-cols-2 min-h-screen place-content-center">
+      <div className=" flex items-center justify-center">
+        <div className="px-3">
+          <div className="text-4xl font-extrabold ">
+            Taxing Laughter: The Joke Tax Chronicles
+          </div>
+          <div className="text-slate-400 pt-2">Posted on Augest 24, 2023</div>
+              </div>
+              <div>
+                  
+              </div>
+      </div>
+      <div className="flex items-center justify-center">
+        <div>hello</div>
+      </div>
     </div>
-}
+  );
+};
