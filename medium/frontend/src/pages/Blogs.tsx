e14b565f4623b197ce1b0affd5427f2c0f8987f6
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2025-07-18 16:00:45 +0530

    styled BlogCard

--- medium/frontend/src/pages/Blogs.tsx
+++ medium/frontend/src/pages/Blogs.tsx
@@ -2,13 +2,35 @@ import { BlogCard } from "../components/BlogCard";
 
 export const Blogs = () => {
   return (
-    <div>
-      <BlogCard
-        authorName={"kishan"}
-        title={"Jorney throught C"}
-        content={"Intro to low level language"}
-        publishedDate={"2 feb 2024"}
-      />
+    <div className="flex justify-center">
+      <div className=" max-w-xl">
+        <BlogCard
+          authorName={"Kishan"}
+          title={"Jorney throught C++ adn the advance concept like Cmake "}
+          content={
+            "Intro to low level language and Memory management how data is stored "
+          }
+          publishedDate={"2 feb 2024"}
+        />
+
+        <BlogCard
+          authorName={"Kishan"}
+          title={"Jorney throught C++ adn the advance concept like Cmake "}
+          content={
+            "Intro to low level language and Memory management how data is stored "
+          }
+          publishedDate={"2 feb 2024"}
+        />
+
+        <BlogCard
+          authorName={"Kishan"}
+          title={"Jorney throught C++ adn the advance concept like Cmake "}
+          content={
+            "Intro to low level language and Memory management how data is stored "
+          }
+          publishedDate={"2 feb 2024"}
+        />
+      </div>
     </div>
   );
 };
