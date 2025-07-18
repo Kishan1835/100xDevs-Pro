5d8d42724f695b68762b96392b475ddf4a9ee1d3
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2025-07-18 16:00:45 +0530

    styled BlogCard

--- medium/frontend/src/components/BlogCard.tsx
+++ medium/frontend/src/components/BlogCard.tsx
@@ -12,30 +12,37 @@ export const BlogCard = ({
   publishedDate,
 }: BlogcardProps) => {
   return (
-    <div>
+    <div className="p-4 border-b border-slate-200 pb-4">
       <div className="flex font-semibold">
-        <div className="flex justify-center flex-col">
-          {<Avatar name={authorName} />}
+        {<Avatar name={authorName} />}
+        <div className="font-extralight pl-2 text-sm flex justify-center flex-col">
+          {" "}
+          {authorName}
         </div>
-        <div className="font-extralight pl-2"> {authorName}</div>
-        <div>&#9679;</div>
-        <div className="pl-2 font-thin text-slate-500"> {publishedDate}</div>
+        <div className="flex justify-center flex-col pl-2">
+          <Circle />
+        </div>
+        <div className="pl-2 font-thin text-slate-500 text-sm flex justify-center flex-col">
+          {publishedDate}
+        </div>
       </div>
-
-      <div className="font-semibold ">
-        <div>{title}</div>
-        <div>{content.slice(0, 100)}+"..."</div>
-        <div>{`${Math.ceil(content.length / 100)} minutes`} </div>
-        <div className="bg-slate-200 h-1 w-full"></div>
+      <div className="font-semibold font-xl pt-2">{title}</div>
+      <div className="font-thin foont-md">{content.slice(0, 100) + "..."}</div>
+      <div className=" text-slate-500 text-sm font-thin pt-4">
+        {`${Math.ceil(content.length / 100)} minute(s) read`}
       </div>
     </div>
   );
 };
 
+function Circle() {
+  return <div className="h-1 w-1 rounded-full bg-slate-500"></div>;
+}
+
 function Avatar({ name }: { name: string }) {
   return (
-    <div className="relative inline-flex items-center justify-center w-4 h-4 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-      <span className="font-medium text-gray-600 dark:text-gray-300">
+    <div className="relative inline-flex items-center justify-center w-4 h-6 overflow-hidden bg-black rounded-full">
+      <span className="font-medium text-slate-100 dark:text-gray-300">
         {name[0]}
       </span>
     </div>
